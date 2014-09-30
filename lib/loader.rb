e3e659d4db234d247a2685cfaec0c136bf544341
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-09-30 16:23:01 -0600

    Added Saver:

--- lib/loader.rb
+++ lib/loader.rb
@@ -6,14 +6,4 @@ class Loader
 		data = CSV.open(path, headers: true, header_converters: :symbol)
 		@attendees = data.map {|row| Entry.new(row)}
 	end
-
-	def save_file(queue, file_name)
-		CSV.open("data/#{file_name}.csv", "wb") do |csv|
-			csv << %w( ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode)
-			queue.each do |entry|
-				# csv << entry.to_a
-				csv << [entry.id, entry.regdate, entry.first_name]
-			end
-		end
-	end
 end
---
+++ test/saver_test.rb
@@ -0,0 +1,18 @@
+require_relative 'test_helper'
+
+class SaverTest < MiniTest::Test 
+
+	def test_saved_a_csv
+		csv 			 = Loader.new("./data/test.csv")
+		repository = Repository.new(csv.attendees)
+		saver 		 = Saver.new
+		attribute  = "first_name"
+		criteria   = "sarah"
+		queue      = repository.find_by(attribute, criteria)
+		file_name  = "test_output_file"
+
+		saver.save_file(queue, file_name)
+
+		assert File.exist?('./data/test_output_file.csv')
+	end
+end
